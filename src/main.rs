a3309f2568cc9dc3df851087803ad8af615b7664
Stephen Karger <stephen.karger@gmail.com>
2019-05-26 18:25:45 -0400

    Make port number based on environment

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use actix_web::{web, App, Responder, HttpServer};
+use std::env;
 
 fn index(info: web::Path<(String, u32)>) -> impl Responder {
     format!("Hello {}! id:{}", info.0, info.1)
 }
 
 fn main() -> std::io::Result<()> {
+    // Get the port number to listen on.
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| App::new().service(
         web::resource("/{name}/{id}/index.html").to(index))
     )
-        .bind("127.0.0.1:8080")?
+        .bind(("0.0.0.0", port))?
         .run()
 }
