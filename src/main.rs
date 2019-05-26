0539d38d7159fa5c52f0da9bd9c31ae1b3c4ef45
Stephen Karger <stephen.karger@gmail.com>
2019-05-26 17:35:55 -0400

    Copy example actix-web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, Responder, HttpServer};
+
+fn index(info: web::Path<(String, u32)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.0, info.1)
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(
+        web::resource("/{name}/{id}/index.html").to(index))
+    )
+        .bind("127.0.0.1:8080")?
+        .run()
 }
