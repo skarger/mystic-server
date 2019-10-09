9ddc0ace94982e9f5de01ca91ebf5e9545a16274
Stephen Karger <stephen.karger@gmail.com>
2019-10-08 22:01:12 -0400

    Restore cssURL

--- web/src/main.rs
+++ web/src/main.rs
@@ -74,6 +74,7 @@ fn search(data: web::Data<AppState>) -> HttpResponse {
       "appEnvironment": format!("{}", env::var("APP_ENVIRONMENT").unwrap()),
       "baseURL": format!("{}", env::var("BASE_URL").unwrap()),
       "scriptURL": format!("{}/app-{}.js", env::var("CLIENT_BASE_URL").unwrap(), env::var("CLIENT_JS_ID").unwrap()),
+      "cssURL": format!("{}/app-{}.css", env::var("CLIENT_BASE_URL").unwrap(), env::var("CLIENT_CSS_ID").unwrap()),
     });
 
     HttpResponse::Ok()
